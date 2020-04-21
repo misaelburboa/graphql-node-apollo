a3c1fe424f2622020b3c680587beacf21b52972d
Misael Burboa <cmburboa@gmail.com>
2020-04-20 21:10:15 -0400

    files reestructuring

---
+++ graphql-basics/src/resolvers/Query.js
@@ -0,0 +1,42 @@
+const Query = {
+    users(parent, args, { db }, info) {
+        if (!args.query) {
+            return db.users;
+        }
+
+        return db.users.filter((user) => {
+            return user.name.toLowerCase().includes(args.query.toLowerCase());
+        });
+    },
+    posts(parent, args, { db }, info) {
+        if (!args.query) {
+            return db.posts;
+        }
+
+        return db.posts.filter((post) => {
+            return post.title.toLowerCase().includes(args.query.toLowerCase()) |
+            post.body.toLowerCase().includes(args.query.toLowerCase())
+        });
+    },
+    comments(parent, args, { db }, info) {
+        return db.comments;
+    },
+    me() {
+        return {
+            id: '1234',
+            name: 'misael',
+            email: 'cmburboa@gmail.com',
+            age: 31
+        };
+    },
+    post() {
+        return {
+            id: '4321',
+            title: 'This is my first Post!',
+            body: 'This is the body of my post!.',
+            published: true
+        };
+    }
+};
+
+export { Query as default };
