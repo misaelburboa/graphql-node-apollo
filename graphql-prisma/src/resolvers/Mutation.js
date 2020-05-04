c3677f033db4cee0d922e6fe0d1e32549a67168c
Misael Burboa <cmburboa@gmail.com>
2020-05-04 09:31:39 -0400

    login functionality added

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -97,6 +97,27 @@ const Mutation = {
                 id: args.id
             }
         }, info);
+    },
+    async login(parent, { data }, { prisma }, info) {
+        const user = await prisma.query.user({
+            where: {
+                email: data.email
+            }
+        });
+
+        if (!user) {
+            throw new Error('Unable to login.');
+        }
+
+        const isMatch = await bcrypt.compare(data.password, user.password);
+        if (!isMatch) {
+            throw new Error('Unable to login.');
+        }
+
+        return {
+            user,
+            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+        };
     }
 };
 
