468de0b66013e5304b959cb360b08724d472918b
Misael Burboa <cmburboa@gmail.com>
2020-05-07 09:03:25 -0400

    update password and expire jwt tokens

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -1,15 +1,32 @@
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import getUserId from '../utils/getUserId';
+import generateToken from '../utils/generateToken';
+import hashPassword from '../utils/hashPassword';
 
 const Mutation = {
-    async createUser(parent, args, { prisma }, info) {
-        if (args.data.password.length < 8) {
-            throw new Error('Password must be 8 characters or longer');
+    async login(parent, { data }, { prisma }, info) {
+        const user = await prisma.query.user({
+            where: {
+                email: data.email
+            }
+        });
+
+        if (!user) {
+            throw new Error('Unable to login.');
         }
 
-        const password = await bcrypt.hash(args.data.password, 10);
+        const isMatch = await bcrypt.compare(data.password, user.password);
+        if (!isMatch) {
+            throw new Error('Unable to login.');
+        }
 
+        return {
+            user,
+            token: generateToken(user.id)
+        };
+    },
+    async createUser(parent, args, { prisma }, info) {
+        const password = await hashPassword(args.data.password);
         const user = await prisma.mutation.createUser({
             data: {
                 ...args.data,
@@ -19,7 +36,7 @@ const Mutation = {
 
         return {
             user,
-            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+            token: generateToken(user.id)
         };
     },
     async deleteUser(parent, args, { prisma, request }, info) {
@@ -37,6 +54,10 @@ const Mutation = {
     },
     async updateUser(parent, args, { prisma, request }, info) {
         const userId = getUserId(request);
+
+        if (typeof args.data.password === 'string') {
+            args.data.password = await hashPassword(args.data.password);
+        }
 
         return prisma.mutation.updateUser({
             data: args.data,
@@ -180,27 +201,6 @@ const Mutation = {
                 id: args.id
             }
         }, info);
-    },
-    async login(parent, { data }, { prisma }, info) {
-        const user = await prisma.query.user({
-            where: {
-                email: data.email
-            }
-        });
-
-        if (!user) {
-            throw new Error('Unable to login.');
-        }
-
-        const isMatch = await bcrypt.compare(data.password, user.password);
-        if (!isMatch) {
-            throw new Error('Unable to login.');
-        }
-
-        return {
-            user,
-            token: jwt.sign({ userId: user.id }, 'thisisasecret')
-        };
     }
 };
 
