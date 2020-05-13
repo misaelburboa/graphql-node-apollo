5164f18609292d6a5ec3f32b1c55290d3cf47a21
Misael Burboa <cmburboa@gmail.com>
2020-05-13 09:12:22 -0400

    order by set up

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -11,7 +11,8 @@ const Query = {
         const operationArgs = {
             first: args.first,
             skip: args.skip,
-            after: args.after
+            after: args.after,
+            orderBy: args.orderBy
         };
 
         if (args.query) {
@@ -29,6 +30,7 @@ const Query = {
             first: args.first,
             skip: args.skip,
             after: args.after,
+            orderBy: args.orderBy,
             where: {
                 published: true
             }
@@ -71,7 +73,8 @@ const Query = {
         const opArgs = {
             first: args.first,
             skip: args.skip,
-            after: args.after
+            after: args.after,
+            orderBy: args.orderBy
         };
 
         return prisma.query.comments(opArgs, info);
