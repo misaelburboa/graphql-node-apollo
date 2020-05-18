5ac37308513f1d09f8023f09f1a56a071ed05d69
Misael Burboa <cmburboa@gmail.com>
2020-05-18 10:01:15 -0400

    setup of apollo client, client creation and jest configuration

--- graphql-prisma/src/index.js
+++ graphql-prisma/src/index.js
@@ -1,28 +1,5 @@
 import '@babel/polyfill/noConflict';
-import { GraphQLServer, PubSub } from 'graphql-yoga';
-import db from './db';
-import { resolvers, fragmentReplacements } from './resolvers/index';
-import prisma from './prisma';
-
-const pubsub = new PubSub();
-
-const server = new GraphQLServer({
-    // provide the type definitions stored in the graphql file
-    typeDefs: './src/schema.graphql',
-    // Here is providing the resolvers
-    resolvers,
-    // Here provides the arguments for being use in the context (ctx)
-    // variable in resolvers so you can access ctx.db for example
-    context(request) {
-        return {
-            db,
-            pubsub,
-            prisma,
-            request
-        };
-    },
-    fragmentReplacements
-});
+import server from './server';
 
 const options = { port: process.env.PORT || 4000 };
 server.start(options, () => {
